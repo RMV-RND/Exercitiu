7bf0924f63aa2b14367148b07d688ab3bfe4126c
RMV-RND <56356819+RMV-RND@users.noreply.github.com>
2023-04-17 12:48:04 +0300

    Add files via upload

---
+++ ExercitiuInterviu/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Exercitiu.numberOfDigitsInIntegerPart(100.0);
+        Exercitiu.numberOfDigitsInIntegerPart(-3.142);
+        Exercitiu.numberOfDigitsInIntegerPart(9999.9999f);
+        Exercitiu.numberOfDigitsInIntegerPart(0);
+    }
+}
